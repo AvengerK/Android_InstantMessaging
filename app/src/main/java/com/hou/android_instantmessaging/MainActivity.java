9a193b99259d221137a83e7d89739d9805c6bcdc
Hou <avenger19970116@gmail.com>
2016-03-13 23:18:36 +0800

    3/13

---
+++ app/src/main/java/com/hou/android_instantmessaging/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hou.android_instantmessaging;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 凌霄 on 2016/3/13.
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
