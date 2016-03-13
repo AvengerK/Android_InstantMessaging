b492be5945a83c17be6d81054d38e8e48f68225e
Hou <avenger19970116@gmail.com>
2016-03-13 23:54:00 +0800

    登录，会话列表，注册的雏形

--- app/src/main/java/com/hou/android_instantmessaging/MainActivity.java
+++ app/src/main/java/com/hou/android_instantmessaging/MainActivity.java
@@ -1,7 +1,10 @@
 package com.hou.android_instantmessaging;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by 凌霄 on 2016/3/13.
@@ -11,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button signinBtn = (Button)findViewById(R.id.signup);
+        Button loginBtn = (Button)findViewById(R.id.login);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,DialogList.class);
+                startActivity(intent);
+            }
+        });
+
+        signinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SignIn.class);
+                startActivity(intent);
+            }
+        });
     }
 }
