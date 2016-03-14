946ed4cc06f6c12e95f3f88a1172f836a836af71
Hou <avenger19970116@gmail.com>
2016-03-14 23:37:23 +0800

    made a simple sign up

--- app/src/main/java/com/hou/android_instantmessaging/MainActivity.java
+++ app/src/main/java/com/hou/android_instantmessaging/MainActivity.java
@@ -1,10 +1,12 @@
 package com.hou.android_instantmessaging;
 
 import android.content.Intent;
+import android.media.MediaExtractor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by 凌霄 on 2016/3/13.
@@ -16,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Button signupBtn = (Button)findViewById(R.id.signup);
+        Button signUpBtn = (Button)findViewById(R.id.signup);
         Button loginBtn = (Button)findViewById(R.id.login);
 
 
@@ -29,13 +31,14 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        signupBtn.setOnClickListener(new View.OnClickListener() {
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SignUp.class);
                 startActivity(intent);
             }
         });
-
     }
+
+
 }
