498b139e0ce00abdb6bd44a50d8ffa0c82d7ff1b
Hou <avenger19970116@gmail.com>
2016-03-15 23:37:23 +0800

    未完成的登录

--- app/src/main/java/com/hou/android_instantmessaging/MainActivity.java
+++ app/src/main/java/com/hou/android_instantmessaging/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by 凌霄 on 2016/3/13.
@@ -17,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final String inputAccount = ((EditText)findViewById(R.id.account)).getText().toString();
+        final String inputPassword = ((EditText)findViewById(R.id.password)).getText().toString();
+
 
         Button signUpBtn = (Button)findViewById(R.id.signup);
         Button loginBtn = (Button)findViewById(R.id.login);
@@ -25,8 +29,9 @@ public class MainActivity extends AppCompatActivity {
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,DialogList.class);
-                startActivity(intent);
+//                Intent intent = new Intent(MainActivity.this,DialogList.class);
+//                startActivity(intent);
+                logIn(inputAccount,inputPassword);
             }
         });
 
@@ -34,14 +39,41 @@ public class MainActivity extends AppCompatActivity {
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SignUp.class);
-                startActivity(intent);
+//                Intent intent = new Intent(MainActivity.this, SignUp.class);
+//                startActivity(intent);
+//                SignUp.actionStart(MainActivity.this,"account","password");
+                Intent intent = new Intent(MainActivity.this,SignUp.class);
+                startActivityForResult(intent, 1);
             }
         });
 
-        String account = ((EditText)findViewById(R.id.account)).getText().toString();
-        String password = ((EditText)findViewById(R.id.password)).getText().toString();
+//        String account = ((EditText)findViewById(R.id.account)).getText().toString();
+//        String password = ((EditText)findViewById(R.id.password)).getText().toString();
     }
 
+    private static String account,password;
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        switch (requestCode){
+            case 1:
+                if(resultCode == RESULT_OK){
+//                    final String account = data.getStringExtra("account");
+//                    final String passworf = data.getStringExtra("password");
+                    account = data.getStringExtra("account");
+                    password = data.getStringExtra("password");
+                }
+                break;
+            default:
+        }
+    }
 
+    protected void logIn(String inputAccount,String inputPassword){
+        if(inputAccount == account && inputPassword == password){
+            Intent intent = new Intent(MainActivity.this,DialogList.class);
+            startActivity(intent);
+        }
+        else{
+            Toast.makeText(MainActivity.this,"Login Failed",Toast.LENGTH_SHORT).show();
+        }
+    }
 }
